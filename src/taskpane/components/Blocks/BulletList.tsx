f51abfb2923407b95664f57adc746007094ac019
Andrea Napoli <andrea.napoli4@studio.unibo.it>
2023-04-08 18:50:57 +0200

    finito Blocks

--- src/taskpane/components/Blocks/BulletList.tsx
+++ src/taskpane/components/Blocks/BulletList.tsx
@@ -5,7 +5,7 @@ import IconButton from '@mui/material/IconButton';
 
 export const BulletList = () => {
 
-  const createBulletList = async () => {
+  const Bullet = async () => {
     await Word.run(async (context) => {
       const selection = context.document.getSelection();
       selection.paragraphs.load();
@@ -31,7 +31,91 @@ export const BulletList = () => {
       }
       await context.sync();
     });
-};
+  }
+
+  const list2 = async () => {
+    await Word.run(async (context) => {
+      const selection = context.document.getSelection();
+      selection.paragraphs.load();
+      await context.sync();
+      
+      if(selection.paragraphs.items[0].isListItem){
+        for (let i = 0; i < selection.paragraphs.items.length; i++) {
+          selection.paragraphs.items[i].delete();
+        }
+        for (let i = 0; i < selection.paragraphs.items.length; i++) {
+          selection.insertText(selection.paragraphs.items[i].text + "\n" , "Before")
+        }
+      }else{
+        const list = selection.paragraphs.items[0].startNewList();
+        list.setLevelBullet(0, Word.ListBullet.diamonds);
+        list.load();
+        await context.sync();
+        for (let i = 1; i < selection.paragraphs.items.length; i++) {
+          list.insertParagraph(selection.paragraphs.items[i].text, "End");
+          list.setLevelBullet(0, Word.ListBullet.diamonds);
+          selection.paragraphs.items[i].delete();
+        }
+      }
+      await context.sync();
+    }); 
+  };
+  
+  const list3 = async () => {
+    await Word.run(async (context) => {
+      const selection = context.document.getSelection();
+      selection.paragraphs.load();
+      await context.sync();
+      
+      if(selection.paragraphs.items[0].isListItem){
+        for (let i = 0; i < selection.paragraphs.items.length; i++) {
+          selection.paragraphs.items[i].delete();
+        }
+        for (let i = 0; i < selection.paragraphs.items.length; i++) {
+          selection.insertText(selection.paragraphs.items[i].text + "\n" , "Before")
+        }
+      }else{
+        const list = selection.paragraphs.items[0].startNewList();
+        list.setLevelBullet(0, Word.ListBullet.square);
+        list.load();
+        await context.sync();
+        for (let i = 1; i < selection.paragraphs.items.length; i++) {
+          list.insertParagraph(selection.paragraphs.items[i].text, "End");
+          list.setLevelBullet(0, Word.ListBullet.square);
+          selection.paragraphs.items[i].delete();
+        }
+      }
+      await context.sync();
+    }); 
+  };
+
+  const list4 = async () => {
+    await Word.run(async (context) => {
+      const selection = context.document.getSelection();
+      selection.paragraphs.load();
+      await context.sync();
+      
+      if(selection.paragraphs.items[0].isListItem){
+        for (let i = 0; i < selection.paragraphs.items.length; i++) {
+          selection.paragraphs.items[i].delete();
+        }
+        for (let i = 0; i < selection.paragraphs.items.length; i++) {
+          selection.insertText(selection.paragraphs.items[i].text + "\n" , "Before")
+        }
+      }else{
+        const list = selection.paragraphs.items[0].startNewList();
+        list.setLevelBullet(0, Word.ListBullet.checkmark);
+        list.load();
+        await context.sync();
+        for (let i = 1; i < selection.paragraphs.items.length; i++) {
+          list.insertParagraph(selection.paragraphs.items[i].text, "End");
+          list.setLevelBullet(0, Word.ListBullet.checkmark);
+          selection.paragraphs.items[i].delete();
+        }
+      }
+      await context.sync();
+    }); 
+  };
 
   return (
     <div>
@@ -46,9 +130,24 @@ export const BulletList = () => {
           spacing={2}
         >
           <Grid item xs={3}>
-          <IconButton color="inherit" title="Bulleted List" onClick={createBulletList}>
-            <FormatListBulletedIcon fontSize="small" />
-          </IconButton>
+            <IconButton color="inherit" title="Bulleted List" onClick={Bullet}>
+              <FormatListBulletedIcon fontSize="small" />
+            </IconButton>
+          </Grid>
+          <Grid item xs={3}>
+            <IconButton color="inherit" title="Diamond List" onClick={list2} style={{position: 'relative', bottom: '3px'}}>
+              <img width={30} src="../../../assets/bulletlist2.png" />
+            </IconButton>
+          </Grid>
+          <Grid item xs={3}>
+            <IconButton color="inherit" title="Square List" onClick={list3} style={{position: 'relative', bottom: '4px'}}>
+              <img width={30} src="../../../assets/squared.png" />
+            </IconButton>
+          </Grid>
+          <Grid item xs={3}>
+            <IconButton color="inherit" title="Check List" onClick={list4} style={{position: 'relative', bottom: '2px'}}>
+              <img width={30} src="../../../assets/checkmark.png" />
+            </IconButton>
           </Grid>
         </Grid>
     </div>
--- src/taskpane/components/Blocks/Nesting.tsx
+++ src/taskpane/components/Blocks/Nesting.tsx
@@ -1,10 +1,72 @@
 import * as React from 'react';
+import { useState, useEffect } from 'react'
 import Grid from '@mui/material/Grid';
 import FormatIndentDecreaseIcon from '@mui/icons-material/FormatIndentDecrease';
 import FormatIndentIncreaseIcon from '@mui/icons-material/FormatIndentIncrease';
 import IconButton from '@mui/material/IconButton';
 
 export const Nesting = () => {
+  let leftMargin = 0;
+  let rightMargin = 0;
+  let interlinea = 5;
+  const Nesting = async () => {
+    await Word.run(async (context) => {
+      const selection = context.document.getSelection();
+      selection.paragraphs.load("style, leftIndent");
+      await context.sync();
+      
+      if(leftMargin < 0){
+        selection.paragraphs.items.forEach((paragraph) => {
+          paragraph.leftIndent -= 30;
+        });
+        leftMargin += 30;
+        rightMargin -= 30;
+      }
+      
+      console.log("leftMargin: ", leftMargin)
+      console.log("rightMargin: ", rightMargin)
+    });
+  }
+
+  const Unnesting = async () => {
+    await Word.run(async (context) => {
+      const selection = context.document.getSelection();
+      selection.paragraphs.load("style, leftIndent");
+      await context.sync();
+      
+      if(rightMargin <= 400){
+        selection.paragraphs.items.forEach((paragraph) => {
+          paragraph.leftIndent += 30;
+      });
+        rightMargin += 30;
+        leftMargin -= 30;
+      }
+      console.log("leftMargin: ", leftMargin)
+      console.log("rightMargin: ", rightMargin)
+    });
+  }
+
+  const lineSpacing = async () => {
+    await Word.run(async (context) => {
+      const selection = context.document.getSelection();
+      selection.paragraphs.load();
+      await context.sync();
+
+      if(interlinea <= 30){
+        interlinea += 5;
+      }else{
+        interlinea = 10;
+      }
+
+      selection.paragraphs.items.forEach((paragraph) => {
+        paragraph.lineSpacing = interlinea;
+      });
+
+      console.log(interlinea)
+      await context.sync();
+    });
+  }
+
     return (
         <div>
             <div style={{display: 'flex', justifyContent: 'center', marginTop: '5px', marginBottom: '5px'}}>
@@ -18,22 +80,22 @@ export const Nesting = () => {
               spacing={1}
             >
               <Grid item xs={4}>
-                <IconButton color="inherit">
+                <IconButton color="inherit" onClick={Nesting}>
                   <FormatIndentDecreaseIcon style={{marginLeft: "20px", position: 'relative', right: '10px'}}/>
                 </IconButton>
-                <div style={{marginLeft: '7px', fontSize: '10px'}}>nested list</div>
+                <div style={{marginLeft: '7px', fontSize: '10px'}}>move left</div>
               </Grid>
               <Grid item xs={4}>
-                <IconButton color="inherit">
+                <IconButton color="inherit" onClick={Unnesting}>
                   <FormatIndentIncreaseIcon style={{marginLeft: "20px", position: 'relative', right: '10px'}}/>
                 </IconButton>
-                <div style={{marginLeft: '6px', fontSize: '10px'}}>unnested list</div>
+                <div style={{marginLeft: '6px', fontSize: '10px'}}>move right</div>
               </Grid>
               <Grid item xs={4}>
-                <IconButton color="inherit">
+                <IconButton color="inherit" onClick={lineSpacing}>
                   <img width={30} src="../../../assets/interstitial.png" style={{marginLeft: "20px", position: 'relative', right: '10px'}} />
                 </IconButton>
-                <div style={{marginLeft: '10px', fontSize: '10px'}}>interstitial</div>
+                <div style={{marginLeft: '10px', fontSize: '10px'}}>line spacing</div>
               </Grid>
             </Grid>
         </div>
