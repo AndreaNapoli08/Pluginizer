b2ff3c17a6aa31f59d08240e6e89be7b403df0e6
Andrea Napoli <andrea.napoli4@studio.unibo.it>
2023-04-08 12:02:47 +0200

    finito bullet list

--- src/taskpane/components/Blocks/BulletList.tsx
+++ src/taskpane/components/Blocks/BulletList.tsx
@@ -4,24 +4,39 @@ import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
 import IconButton from '@mui/material/IconButton';
 
 export const BulletList = () => {
-    return (
-        <div>
-            <div style={{display: 'flex', justifyContent: 'center', marginTop: '5px', marginBottom: '5px'}}>
-                Bullet Lists:
-            </div>
-            <Grid
-              container
-              direction="row"
-              justifyContent="left"
-              alignItems="flex-start"
-              spacing={2}
-            >
-              <Grid item xs={3}>
-              <IconButton color="inherit" title="Bulleted List">
-                <FormatListBulletedIcon fontSize="small" />
-              </IconButton>
-              </Grid>
-            </Grid>
+
+  const createBulletList = async () => {
+    await Word.run(async (context) => {
+      const selection = context.document.getSelection();
+      selection.paragraphs.load();
+      await context.sync();
+  
+      selection.paragraphs.items.forEach(async (paragraph) => {
+          await paragraph.startNewList();
+      });
+      
+      await context.sync();
+    });
+};
+
+  return (
+    <div>
+        <div style={{display: 'flex', justifyContent: 'center', marginTop: '5px', marginBottom: '5px'}}>
+            Bullet Lists:
         </div>
-    )
+        <Grid
+          container
+          direction="row"
+          justifyContent="left"
+          alignItems="flex-start"
+          spacing={2}
+        >
+          <Grid item xs={3}>
+          <IconButton color="inherit" title="Bulleted List" onClick={createBulletList}>
+            <FormatListBulletedIcon fontSize="small" />
+          </IconButton>
+          </Grid>
+        </Grid>
+    </div>
+  )
 }
