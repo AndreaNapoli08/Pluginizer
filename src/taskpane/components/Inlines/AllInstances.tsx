47f9e7179220bc6bc7422c1cde5dc93c0ac074c9
Andrea Napoli <andrea.napoli4@studio.unibo.it>
2023-04-16 13:02:43 +0200

    migliorato

--- src/taskpane/components/Inlines/AllInstances.tsx
+++ src/taskpane/components/Inlines/AllInstances.tsx
@@ -12,46 +12,40 @@ export const AllInstances  = ({selectedText, buttonStyle, firstOccurence}) => {
         setAllInstances(event.target.checked);
     };
     
-    if(allInstances){
-        const applyFormatting = async (context) => {
-            const range = context.document.body.getRange();
-            const searchResults = range.search(selectedText, { matchCase: false, matchWholeWord: false });
-            searchResults.load("items");
-            await context.sync();
-        
-            const occurrences = searchResults.items;
-
-            occurrences.forEach((occurrence) => {
-                if(fontStyle == "bold"){
-                  if(firstOccurence){
-                    occurrence.font.bold = false;
-                  }else{
-                    occurrence.font.bold = true;
-                  }
-                }else if(fontStyle == "italic"){
-                  if(firstOccurence){
-                    occurrence.font.italic = false;
-                  }else{
-                    occurrence.font.italic = true;
-                  }
-                }else if(fontStyle == "underline"){
-                  if(firstOccurence == "Single"){
-                    occurrence.font.underline = "None";
-                  }else{
-                    occurrence.font.underline = "Single";
-                  }
-                }
-            });
-        
-            await context.sync();
-        };
-
-        Word.run(async (context) => {
-            await applyFormatting(context);
-        }).catch((error) => {
-        console.error(error);
+    if (allInstances) {
+      const applyFormatting = async (context) => {
+        const range = context.document.body.getRange();
+        const searchResults = range.search(selectedText, { matchCase: false, matchWholeWord: false });
+        searchResults.load("items");
+        await context.sync();
+    
+        const occurrences = searchResults.items;
+    
+        occurrences.forEach((occurrence) => {
+          switch (fontStyle) {
+            case "bold":
+              occurrence.font.bold = !firstOccurence;
+              break;
+            case "italic":
+              occurrence.font.italic = !firstOccurence;
+              break;
+            case "underline":
+              occurrence.font.underline = firstOccurence === "Single" ? "None" : "Single";
+              break;
+            default:
+              break;
+          }
         });
-    }
+    
+        await context.sync();
+      };
+    
+      Word.run(async (context) => {
+        await applyFormatting(context);
+      }).catch((error) => {
+        console.error(error);
+      });
+    }    
 
     return (
         <div>
