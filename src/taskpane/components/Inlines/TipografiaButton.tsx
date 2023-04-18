7cc6ff3452ac30f53e791515133238308386774f
Andrea Napoli <andrea.napoli4@studio.unibo.it>
2023-04-18 12:53:13 +0200

    funziona cazzo

--- src/taskpane/components/Inlines/TipografiaButton.tsx
+++ src/taskpane/components/Inlines/TipografiaButton.tsx
@@ -3,63 +3,127 @@ import { useState, useEffect } from 'react';
 import IconButton from '@mui/material/IconButton';
 
 export const TipografiaButton = ({setDis, onFirstOccurence, onButtonStyle, expandedText}) => {
-
+ 
   const isLetterOrNumber = (char) => {
-    return /^[a-zA-Z0-9]+$/.test(char);
+    if (typeof char === "undefined") {
+      return false;
+    }else{
+      return /^[a-zA-Z0-9]+$/.test(char);
+    }
+    
   }
 
   const toggleFontStyle = async (style) => {
     await Word.run(async (context) => {
-      const selection = context.document.getSelection();
+      let selection = context.document.getSelection();
+      
       selection.load("text, font");
       await context.sync();
       if (selection.isNullObject) {
         return;
       }
 
-      if (selection.text !== expandedText) {
-        let selectionText = selection.text;
-        const startIndex = expandedText.indexOf(selectionText);
-        const endIndex = startIndex + selectionText.length - 1;
-      
-        if(startIndex >= 0){
-          const charBefore = expandedText[startIndex - 1]; 
-          const charAfter = expandedText[endIndex + 1];
-          //controllo se dopo la parte selezionata ci sono lettere
-          if(expandedText[endIndex] == " "){
-          }else{
-            if(isLetterOrNumber(charAfter)){
-              let currentIndex = endIndex + 1;
-              while (currentIndex < expandedText.length) {
-                const currentChar = expandedText[currentIndex];
-                if (isLetterOrNumber(currentChar)) {
-                  // currentChar.font.bold=true; non funziona
-                  currentIndex++;
-                } else {
-                  break;
-                }
-              }
-            }
-          }
-          //controllo se prima della parte selezionata ci sono lettere
-          if(expandedText[startIndex] == " " || startIndex == 0){
-          }else{
-            if(isLetterOrNumber(charBefore)){
-              let currentIndex = startIndex - 1;
-              while (currentIndex >= 0) {
-                const currentChar = expandedText[currentIndex];
-                if (isLetterOrNumber(currentChar)) {
-                  // currentChar.font.bold=true; non funziona
-                  currentIndex--;
-                } else {
-                  break;
-                }
-              }
-            }   
-          }
+
+      // https://stackoverflow.com/questions/58357313/getting-a-ranges-surrounding-text-in-office-js
+      // https://stackoverflow.com/questions/51159644/word-js-apis-extending-a-range
+
+      // Expand to end of sentence
+      if(expandedText != selection.text){
+        const startIndex = expandedText.indexOf(selection.text);
+        const charBefore = expandedText[startIndex - 1];
+        let text = selection.text;
+        let spaceCount = text.split(" ").length;
+       
+        //selezione in avanti fino ad uno di quei caratteri
+        let rngNextSent = selection.getNextTextRangeOrNullObject([".", " ", ","]);
+        selection = selection.expandToOrNullObject(rngNextSent.getRange("Start"));
+        
+        await context.sync();
+        // selezione all'indietro
+        
+        if(isLetterOrNumber(charBefore)){
+          let paragraph = selection.paragraphs.getFirst();
+          paragraph.load("text");
+          await context.sync();
+
+          let rangeToSelect = paragraph.getRange("Start").expandTo(selection);
+          let textBeforeSelection = rangeToSelect.getTextRanges([" ", ".", ",", ";"], false);
+          textBeforeSelection.load("items");
+          await context.sync();
+          let lastItem = textBeforeSelection.items[textBeforeSelection.items.length - spaceCount];
+          let rangeToExpand = lastItem.getRange("Start");
+          selection = selection.expandToOrNullObject(rangeToExpand);
+
+          await context.sync();
+
+          /*let rngDocStart = selection.parentBody.getRange("Start");
+          let rngToStart = selection.expandToOrNullObject(rngDocStart);
+          let paras = rngToStart.paragraphs;
+          paras.load("items");
+          await context.sync();
+          let nrParas = rngToStart.paragraphs.items.length-1;
+          let rngPrev = rngToStart.paragraphs.items[nrParas];
+
+          // Get the last sentence in that paragraph
+          let rngParaSent = rngPrev.getTextRanges([".", " ", "/n"]);
+          rngParaSent.load("items"); 
+          await context.sync();
+          let rngPrevSent = rngParaSent.items[0];
+
+          // Extend the extended range to the last sentence in the previous paragraph
+          selection = selection.expandToOrNullObject(rngPrevSent);*/
         }
-        selection.insertText(expandedText, "Replace");
+        
+        selection.select();
+        selection.load("text, font");
+        await context.sync();
       }
+      
+
+      
+      // if (selection.text !== expandedText) {
+      //   let selectionText = selection.text;
+      //   const startIndex = expandedText.indexOf(selectionText);
+      //   const endIndex = startIndex + selectionText.length - 1;
+      
+      //   if(startIndex >= 0){
+      //     const charBefore = expandedText[startIndex - 1]; 
+      //     const charAfter = expandedText[endIndex + 1];
+      //     //controllo se dopo la parte selezionata ci sono lettere
+      //     if(expandedText[endIndex] == " "){
+      //     }else{
+      //       if(isLetterOrNumber(charAfter)){
+      //         let currentIndex = endIndex + 1;
+      //         while (currentIndex < expandedText.length) {
+      //           const currentChar = expandedText[currentIndex];
+      //           if (isLetterOrNumber(currentChar)) {
+      //             // currentChar.font.bold=true; non funziona
+      //             currentIndex++;
+      //           } else {
+      //             break;
+      //           }
+      //         }
+      //       }
+      //     }
+      //     //controllo se prima della parte selezionata ci sono lettere
+      //     if(expandedText[startIndex] == " " || startIndex == 0){
+      //     }else{
+      //       if(isLetterOrNumber(charBefore)){
+      //         let currentIndex = startIndex - 1;
+      //         while (currentIndex >= 0) {
+      //           const currentChar = expandedText[currentIndex];
+      //           if (isLetterOrNumber(currentChar)) {
+      //             // currentChar.font.bold=true; non funziona
+      //             currentIndex--;
+      //           } else {
+      //             break;
+      //           }
+      //         }
+      //       }   
+      //     }
+      //   }
+      //   selection.insertText(expandedText, "Replace");
+      // }
 
       switch (style) {
         case 'bold':
