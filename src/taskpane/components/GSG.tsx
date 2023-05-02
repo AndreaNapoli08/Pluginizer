fbaeba55fdbc287033ccb939687395bb0cabef22
Andrea Napoli <andrea.napoli4@studio.unibo.it>
2023-05-02 12:49:56 +0200

    cambiati percorsi immagine

--- src/taskpane/components/GSG.tsx
+++ src/taskpane/components/GSG.tsx
@@ -249,103 +249,103 @@ export const GSG = ({expandedText, onFirstStyleGSG, onUpdateStyleGSG}) => {
         <div>
             <div>
             <IconButton color="inherit" style={{borderRadius: '10px', textAlign: "left"}} onClick={() => updateStyleGSG(1)}>
-                <img src="../../../../assets/GSG1.png" width={40}/>
+                <img src="assets/GSG1.png" width={40}/>
                 <span style={{fontSize: "16px", marginLeft: "15px", fontFamily: "cursive"}}>1 No poverty</span>
             </IconButton>
             </div>
             <div>
                 <IconButton color="inherit" style={{borderRadius: '10px', textAlign: "left"}} onClick={() => updateStyleGSG(2)}>
-                    <img src="../../../../assets/GSG2.png" width={40}/>
+                    <img src="assets/GSG2.png" width={40}/>
                     <span style={{fontSize: "16px", marginLeft: "15px", fontFamily: "cursive"}}>2 Zero Hunger</span>
                 </IconButton>
             </div>
             <div>
                 <IconButton color="inherit" style={{borderRadius: '10px', textAlign: "left"}} onClick={() => updateStyleGSG(3)}>
-                    <img src="../../../../assets/GSG3.png" width={40}/>
+                    <img src="assets/GSG3.png" width={40}/>
                     <span style={{fontSize: "16px", marginLeft: "15px", fontFamily: "cursive"}}>3 Good health and weel-being</span>
                 </IconButton>
             </div>
             <div>
                 <IconButton color="inherit" style={{borderRadius: '10px', textAlign: "left"}} onClick={() => updateStyleGSG(4)}>
-                    <img src="../../../../assets/GSG4.png" width={40}/>
+                    <img src="assets/GSG4.png" width={40}/>
                     <span style={{fontSize: "16px", marginLeft: "15px", fontFamily: "cursive"}}>4 Quality education</span>
                 </IconButton>
             </div>
             <div>
                 <IconButton color="inherit" style={{borderRadius: '10px', textAlign: "left"}} onClick={() => updateStyleGSG(5)}>
-                    <img src="../../../../assets/GSG5.png" width={40}/>
+                    <img src="assets/GSG5.png" width={40}/>
                     <span style={{fontSize: "16px", marginLeft: "15px", fontFamily: "cursive"}}>5 Gender equality</span>
                 </IconButton>
             </div>
             <div>
                 <IconButton color="inherit" style={{borderRadius: '10px', textAlign: "left"}} onClick={() => updateStyleGSG(6)}>
-                    <img src="../../../../assets/GSG6.png" width={40}/>
+                    <img src="assets/GSG6.png" width={40}/>
                     <span style={{fontSize: "16px", marginLeft: "15px", fontFamily: "cursive"}}>6 Clean water and sanitation</span>
                 </IconButton>
             </div>
             <div>
                 <IconButton color="inherit" style={{borderRadius: '10px', textAlign: "left"}} onClick={() => updateStyleGSG(7)}>
-                    <img src="../../../../assets/GSG7.png" width={40}/>
+                    <img src="assets/GSG7.png" width={40}/>
                     <span style={{fontSize: "16px", marginLeft: "15px", fontFamily: "cursive"}}>7 Affordable and clean energy</span>
                 </IconButton>
             </div>
             <div>
                 <IconButton color="inherit" style={{borderRadius: '10px', textAlign: "left"}} onClick={() => updateStyleGSG(8)}>
-                    <img src="../../../../assets/GSG8.png" width={40}/>
+                    <img src="assets/GSG8.png" width={40}/>
                     <span style={{fontSize: "16px", marginLeft: "15px", fontFamily: "cursive"}}>8 Decent Work And economic growth</span>
                 </IconButton>
             </div>
             <div>
                 <IconButton color="inherit" style={{borderRadius: '10px', textAlign: "left"}} onClick={() => updateStyleGSG(9)}>
-                    <img src="../../../../assets/GSG9.png" width={40}/>
+                    <img src="assets/GSG9.png" width={40}/>
                     <span style={{fontSize: "16px", marginLeft: "15px", fontFamily: "cursive"}}>9 Industry, innovation and infrastructure</span>
                 </IconButton>
             </div>
             <div>
                 <IconButton color="inherit" style={{borderRadius: '10px', textAlign: "left"}} onClick={() => updateStyleGSG(10)}>
-                    <img src="../../../../assets/GSG10.png" width={40}/>
+                    <img src="assets/GSG10.png" width={40}/>
                     <span style={{fontSize: "16px", marginLeft: "15px", fontFamily: "cursive"}}>10 Reduced inequalities</span>
                 </IconButton>
             </div>
             <div>
                 <IconButton color="inherit" style={{borderRadius: '10px', textAlign: "left"}} onClick={() => updateStyleGSG(11)}>
-                    <img src="../../../../assets/GSG11.png" width={40}/>
+                    <img src="assets/GSG11.png" width={40}/>
                     <span style={{fontSize: "16px", marginLeft: "15px", fontFamily: "cursive"}}>11 Sustainable cities and communities</span>
                 </IconButton>
             </div>
             <div>
                 <IconButton color="inherit" style={{borderRadius: '10px', textAlign: "left"}} onClick={() => updateStyleGSG(12)}>
-                    <img src="../../../../assets/GSG12.png" width={40}/>
+                    <img src="assets/GSG12.png" width={40}/>
                     <span style={{fontSize: "16px", marginLeft: "15px", fontFamily: "cursive"}}>12 Responsible consumption and production</span>
                 </IconButton>
             </div>
             <div>
                 <IconButton color="inherit" style={{borderRadius: '10px', textAlign: "left"}} onClick={() => updateStyleGSG(13)}>
-                    <img src="../../../../assets/GSG13.png" width={40}/>
+                    <img src="assets/GSG13.png" width={40}/>
                     <span style={{fontSize: "16px", marginLeft: "15px", fontFamily: "cursive"}}>13 Climate action</span>
                 </IconButton>
             </div>
             <div>
                 <IconButton color="inherit" style={{borderRadius: '10px', textAlign: "left"}} onClick={() => updateStyleGSG(14)}>
-                    <img src="../../../../assets/GSG14.png" width={40}/>
+                    <img src="assets/GSG14.png" width={40}/>
                     <span style={{fontSize: "16px", marginLeft: "15px", fontFamily: "cursive"}}>14 Life below water</span>
                 </IconButton>
             </div>
             <div>
                 <IconButton color="inherit" style={{borderRadius: '10px', textAlign: "left"}} onClick={() => updateStyleGSG(15)}>
-                    <img src="../../../../assets/GSG15.png" width={40}/>
+                    <img src="assets/GSG15.png" width={40}/>
                     <span style={{fontSize: "16px", marginLeft: "15px", fontFamily: "cursive"}}>15 Life on land</span>
                 </IconButton>
             </div>
             <div>
                 <IconButton color="inherit" style={{borderRadius: '10px', textAlign: "left"}} onClick={() => updateStyleGSG(16)}>
-                    <img src="../../../../assets/GSG16.png" width={40}/>
+                    <img src="assets/GSG16.png" width={40}/>
                     <span style={{fontSize: "16px", marginLeft: "15px", fontFamily: "cursive"}}>16 Peace, Justice and strong institutions</span>
                 </IconButton>
             </div>
             <div>
                 <IconButton color="inherit" style={{borderRadius: '10px', textAlign: "left"}} onClick={() => updateStyleGSG(17)}>
-                    <img src="../../../../assets/GSG17.png" width={40}/>
+                    <img src="assets/GSG17.png" width={40}/>
                     <span style={{fontSize: "16px", marginLeft: "15px", fontFamily: "cursive"}}>17 Partnerships for the goals</span>
                 </IconButton>
             </div>
