da3c5b2e400427ea82aa32deb0ee168fae5a01cb
Andrea Napoli <andrea.napoli4@studio.unibo.it>
2023-08-25 12:53:19 +0200

    finiti documents

--- src/taskpane/components/Documents.tsx
+++ src/taskpane/components/Documents.tsx
@@ -1,52 +1,295 @@
 import { FormControl, Grid, InputLabel, MenuItem, Input, IconButton, Button } from '@mui/material';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 import * as React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
 import { makeStyles } from '@mui/styles';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import DeleteIcon from '@mui/icons-material/Delete';
 
 export const Documents = () => {
-    const [resolution, setResolution] = useState('');
+    const [resolution, setResolution] = useState("decisions");
+    const [language, setLanguage] = useState("");
+    const [stage, setStage] = useState("first")
+    const [office, setOffice] = useState("");
     const [identifier, setIdentifier] = useState('');
     const [valDate, setValDate] = useState('');
-    const [inputFields, setInputFields] = useState([{ value: '' }]);
+    const [drafter, setDrafter] = useState("");
+    const [inputFields, setInputFields] = useState([]);
+    const [showConfirm, setShowConfirm] = useState(false);
+    const [showAddAlias, setShowAddAlias] = useState(true);
+    const [currentInputValue, setCurrentInputValue] = useState('');
+    const [currentIndex, setCurrentIndex] = useState('');
+    let contextDocument;
+    const NAMESPACE_URI = "prova";
+
+    const deleteInformation = async (type) => {
+        if (type === "calendar") {
+            setValDate("");
+        }
+        // Elimina informazione attuale
+        Office.context.document.customXmlParts.getByNamespaceAsync(NAMESPACE_URI, (result) => {
+            if (result.status === Office.AsyncResultStatus.Succeeded) {
+                const xmlParts = result.value;
+                for (const xmlPart of xmlParts) {
+                    xmlPart.getXmlAsync(asyncResult => {
+                        if (asyncResult.status === Office.AsyncResultStatus.Succeeded) {
+                            const xmlData = asyncResult.value;
+                            if (xmlData.includes(`${type}=`)) {
+                                xmlPart.deleteAsync();
+                            }
+                        } else {
+                            console.error("Errore nel recupero dei contenuti personalizzati");
+                        }
+
+                    });
+                }
+            } else {
+                console.error("Errore nel recupero dei contenuti personalizzati");
+            }
+        });
+
+        await contextDocument.sync();
+    }
+
+    const deleteInformationAlias = async (index) => {
+        // Elimina informazione attuale
+        Office.context.document.customXmlParts.getByNamespaceAsync(NAMESPACE_URI, (result) => {
+            if (result.status === Office.AsyncResultStatus.Succeeded) {
+                const xmlParts = result.value;
+                for (const xmlPart of xmlParts) {
+                    xmlPart.getXmlAsync(asyncResult => {
+                        if (asyncResult.status === Office.AsyncResultStatus.Succeeded) {
+                            const xmlData = asyncResult.value;
+                            console.log(xmlData)
+                            console.log(index);
+                            if (xmlData.includes(`index="${index}"`) || xmlData.includes(`index=""`)) {
+                                console.log("Eliminazioneeee :", xmlData)
+                                xmlPart.deleteAsync();
+                            }
+                        } else {
+                            console.error("Errore nel recupero dei contenuti personalizzati");
+                        }
+
+                    });
+                }
+            } else {
+                console.error("Errore nel recupero dei contenuti personalizzati");
+            }
+        });
+
+        await contextDocument.sync();
+    }
+
+    const insertInformation = async (xmlData) => {
+        // inserimento nuova informazione
+        Office.context.document.customXmlParts.addAsync(xmlData, (result) => {
+            if (result.status === Office.AsyncResultStatus.Succeeded) {
+                console.log("Dati personalizzati aggiunti con successo");
+            } else {
+                console.error("Errore durante l'aggiunta dei dati personalizzati");
+            }
+        });
+        await contextDocument.sync();
+    }
+
+    const getInformation = async (type) => {
+        const values = [...inputFields];
+        Office.context.document.customXmlParts.getByNamespaceAsync(NAMESPACE_URI, async (result) => {
+            if (result.status === Office.AsyncResultStatus.Succeeded) {
+                const xmlParts = result.value;
+                for (const xmlPart of xmlParts) {
+                    await xmlPart.getXmlAsync(asyncResult => {    // questa istruzione non aspetta il completamento di ciascuna chiamata
+                        if (asyncResult.status === Office.AsyncResultStatus.Succeeded) {
+                            const xmlData = asyncResult.value;
+                            if (xmlData.includes(`${type}=`)) {
+                                const parser = new DOMParser();
+                                const xmlDoc = parser.parseFromString(xmlData, "text/xml");
+                                const dataElement = xmlDoc.querySelector(`data[${type}]`);
+                                if (dataElement) {
+                                    let jsonData = JSON.parse(dataElement.textContent);
+                                    switch (type) {
+                                        case "resolutions":
+                                            setResolution(jsonData.resolutions);
+                                            break;
+                                        case "language":
+                                            setLanguage(jsonData.language);
+                                            break;
+                                        case "identifier":
+                                            setIdentifier(jsonData.identifier);
+                                            break;
+                                        case "alias":
+                                            values.push({ value: jsonData.alias });
+                                            console.log(values);
+                                            setInputFields(values);
+                                        case "stage":
+                                            setStage(jsonData.stage);
+                                            break;
+                                        case "calendar":
+                                            setValDate(jsonData.calendar);
+                                            break;
+                                        case "office":
+                                            setOffice(jsonData.office);
+                                            break;
+                                    }
+                                }
+                            }
+                        } else {
+                            console.error("Errore nel recupero dei contenuti personalizzati");
+                        }
+                    });
+                }
+            } else {
+                console.error("Errore nel recupero dei contenuti personalizzati");
+            }
+        });
+    }
+
+    useEffect(() => {
+        // Funzione asincrona separata per eseguire Word.run
+        const runWordAsync = async () => {
+            try {
+                await Word.run(async (context) => {
+                    contextDocument = context;
+                    if (Office.context.platform !== Office.PlatformType.OfficeOnline) {
+                        let selection = context.document.getSelection();
+                        selection.load("text");
+                        await context.sync();
+                        selection.insertField("Start", Word.FieldType.author)
+                        selection.load("text");
+                        await context.sync();
+                        setDrafter(selection.text);
+                        selection.clear();
+                    }
+                    getInformation("resolutions");
+                    getInformation("language");
+                    getInformation("identifier");
+                    getInformation("alias");
+                    getInformation("stage");
+                    getInformation("calendar")
+                    getInformation("office");
+                });
+            } catch (error) {
+                console.error(error);
+            }
+        };
+        runWordAsync();
+    }, []);
 
     const handleInputChange2 = (index, event) => {
         const values = [...inputFields];
         values[index].value = event.target.value;
+        setCurrentInputValue(event.target.value);
+        setCurrentIndex(index);
         setInputFields(values);
     };
 
+    const confirm = () => {
+        setShowConfirm(false);
+        setShowAddAlias(true);
+        let jsonData = {
+            alias: currentInputValue,
+            index: currentIndex
+        };
+        const xmlData = `<root xmlns="${NAMESPACE_URI}"><data index="${currentIndex}" alias="${currentInputValue}">${JSON.stringify(jsonData)}</data></root>`;
+        insertInformation(xmlData);
+    }
+
     const handleAddFields = () => {
+        setShowConfirm(true);
         const values = [...inputFields];
         values.push({ value: '' });
         setInputFields(values);
+        setShowAddAlias(false);
     };
 
     const handleRemoveFields = (index) => {
         const values = [...inputFields];
         values.splice(index, 1);
         setInputFields(values);
+        deleteInformationAlias(index);
     };
 
     const useStyles = makeStyles({
         datePicker: {
-            borderRadius: '10px',
+            borderRadius: '5px',
+            border: '1px solid #ccc', // Colore di default del bordo
+            transition: 'border-color 0.3s ease-in-out', // Aggiungi una transizione al cambio di colore del bordo
+            '& input[type="date"]': {
+                border: '1px solid #ccc', // Colore di default del bordo dell'input
+                transition: 'border-color 0.3s ease-in-out', // Aggiungi una transizione al cambio di colore del bordo
+                '&:focus': {
+                    border: '1px solid black', // Cambio di colore del bordo dell'input quando è in focus
+                },
+            },
         },
     });
     const classes = useStyles();
 
     const handleChangeDocumentType = async (event: SelectChangeEvent) => {
+        let jsonData = {
+            resolutions: event.target.value
+        };
+        const uniqueId = Date.now();
+        const xmlData = `<root xmlns="${NAMESPACE_URI}"><data id="${uniqueId}" resolutions="${event.target.value}">${JSON.stringify(jsonData)}</data></root>`;
+        deleteInformation("resolutions");
+        insertInformation(xmlData);
         setResolution(event.target.value);
-    }
+    };
+
+    const handleChangeLanguage = async (event: SelectChangeEvent) => {
+        let jsonData = {
+            language: event.target.value
+        };
+        const uniqueId = Date.now();
+        const xmlData = `<root xmlns="${NAMESPACE_URI}"><data id="${uniqueId}" language="${event.target.value}">${JSON.stringify(jsonData)}</data></root>`;
+        deleteInformation("language");
+        insertInformation(xmlData);
+        setLanguage(event.target.value);
+    };
 
     const handleInputChange = (event) => {
+        let jsonData = {
+            identifier: event.target.value
+        };
+        const uniqueId = Date.now();
+        const xmlData = `<root xmlns="${NAMESPACE_URI}"><data id="${uniqueId}" identifier="${event.target.value}">${JSON.stringify(jsonData)}</data></root>`;
+        deleteInformation("identifier");
+        insertInformation(xmlData);
         setIdentifier(event.target.value);
+    };
+
+    const handleChangeStage = async (event: SelectChangeEvent) => {
+        let jsonData = {
+            stage: event.target.value
+        };
+        const uniqueId = Date.now();
+        const xmlData = `<root xmlns="${NAMESPACE_URI}"><data id="${uniqueId}" stage="${event.target.value}">${JSON.stringify(jsonData)}</data></root>`;
+        deleteInformation("stage");
+        insertInformation(xmlData);
+        setStage(event.target.value);
+    };
+
+    const handleChangeDate = async (newValue) => {
+        let jsonData = {
+            calendar: newValue
+        };
+        const uniqueId = Date.now();
+        const xmlData = `<root xmlns="${NAMESPACE_URI}"><data id="${uniqueId}" calendar="${newValue}">${JSON.stringify(jsonData)}</data></root>`;
+        deleteInformation("calendar");
+        insertInformation(xmlData);
+        setValDate(newValue);
+    }
+
+    const handleChangeOffice = async (event: SelectChangeEvent) => {
+        let jsonData = {
+            office: event.target.value
+        };
+        const uniqueId = Date.now();
+        const xmlData = `<root xmlns="${NAMESPACE_URI}"><data id="${uniqueId}" office="${event.target.value}">${JSON.stringify(jsonData)}</data></root>`;
+        deleteInformation("office");
+        insertInformation(xmlData);
+        setOffice(event.target.value);
     };
 
     return (
@@ -74,9 +317,6 @@ export const Documents = () => {
                             label="resolutions"
                             onChange={handleChangeDocumentType}
                         >
-                            <MenuItem value="">
-                                <em>None</em>
-                            </MenuItem>
                             <MenuItem value="decisions">Decisions</MenuItem>
                             <MenuItem value="report">Report</MenuItem>
                             <MenuItem value="agenda">Agenda</MenuItem>
@@ -109,9 +349,9 @@ export const Documents = () => {
                         <Select
                             labelId="demo-select-small"
                             id="demo-select-small"
-                            value={resolution}
+                            value={language}
                             label="resolutions"
-                            onChange={handleChangeDocumentType}
+                            onChange={handleChangeLanguage}
                         >
                             <MenuItem value="">
                                 <em>None</em>
@@ -120,7 +360,7 @@ export const Documents = () => {
                             <MenuItem value="french">French</MenuItem>
                             <MenuItem value="russian">Russian</MenuItem>
                             <MenuItem value="spanish">Spanish</MenuItem>
-                            <MenuItem value="arabic">Srabic</MenuItem>
+                            <MenuItem value="arabic">Arabic</MenuItem>
                             <MenuItem value="chinese">Chinese</MenuItem>
                         </Select>
                     </FormControl>
@@ -171,9 +411,14 @@ export const Documents = () => {
                     </IconButton>
                 </div>
             ))}
-            <Button onClick={handleAddFields}>
+            {showAddAlias ? <Button color="inherit" onClick={handleAddFields}>
                 Add alias
-            </Button>
+            </Button> : null}
+
+            {showConfirm ? <Button color="primary" onClick={confirm}>
+                Confirm
+            </Button> : null}
+
             <hr />
             <div style={{ display: 'flex', justifyContent: 'center', marginBottom: '10px', fontSize: '20px' }}>
                 Current draft
@@ -198,13 +443,10 @@ export const Documents = () => {
                         <Select
                             labelId="demo-select-small"
                             id="demo-select-small"
-                            value={resolution}
+                            value={stage}
                             label="informal"
-                            onChange={handleChangeDocumentType}
+                            onChange={handleChangeStage}
                         >
-                            <MenuItem value="">
-                                <em>None</em>
-                            </MenuItem>
                             <MenuItem value="first">First draft</MenuItem>
                             <MenuItem value="under">Under revision</MenuItem>
                             <MenuItem value="approved">Approved</MenuItem>
@@ -219,34 +461,34 @@ export const Documents = () => {
                 justifyContent="center"
                 alignItems="flex-start"
                 spacing={1}
-                style={{ marginTop: '5px' }}
             >
                 <Grid item xs={5}>
                     <p style={{ marginLeft: '3px', fontSize: '17px' }}>Completed</p>
                 </Grid>
                 <Grid item xs={7}>
-                    <LocalizationProvider dateAdapter={AdapterDayjs}>
-                        <DatePicker
-                            value={valDate}
-                            onChange={(newValue) => setValDate(newValue)}
-                            className={classes.datePicker}
-                        />
-                    </LocalizationProvider>
+                    <input
+                        title="calendar"
+                        type="date"
+                        value={valDate}
+                        onChange={(event) => handleChangeDate(event.target.value)}
+                        className={classes.datePicker}
+                        style={{ marginLeft: "7px", marginTop: "9px", height: "38px", width: "150px", fontSize: "15px", textAlign: "center" }}
+                    />
                 </Grid>
             </Grid>
-            {valDate != "" ?
+            {valDate !== "" ? (
                 <Grid container justifyContent="flex-end">
                     <Button
                         variant="text"
                         size="small"
                         startIcon={<DeleteIcon />}
-                        onClick={() => setValDate("")}
+                        onClick={() => deleteInformation("calendar")}
                         color="inherit"
                     >
                         Clear
                     </Button>
                 </Grid>
-                : null}
+            ) : null}
             <Grid
                 container
                 direction="row"
@@ -260,11 +502,11 @@ export const Documents = () => {
                 </Grid>
                 <Grid item xs={7}>
                     <input
+                        disabled={true}
                         type="text"
                         id="drafter"
                         name="drafter"
-                        value={identifier}
-                        onChange={handleInputChange}
+                        value={drafter}
                         style={{
                             width: '95%',
                             height: '30px',
@@ -298,9 +540,9 @@ export const Documents = () => {
                         <Select
                             labelId="demo-select-small"
                             id="demo-select-small"
-                            value={resolution}
+                            value={office}
                             label="external draft"
-                            onChange={handleChangeDocumentType}
+                            onChange={handleChangeOffice}
                         >
                             <MenuItem value="">
                                 <em>None</em>
